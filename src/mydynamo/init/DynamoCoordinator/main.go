5577120cc00af5bc55263736d17fc26223b3ee12
wuqiyao20160118 <287926145@qq.com>
2020-12-04 09:07:24 +0800

    Update the starter code which may contain initialization bugs.

--- src/mydynamo/init/DynamoCoordinator/main.go
+++ src/mydynamo/init/DynamoCoordinator/main.go
@@ -54,6 +54,9 @@ func main() {
 	//Use a waitgroup to ensure that we don't exit this goroutine until all servers have exited
 	wg := new(sync.WaitGroup)
 	wg.Add(cluster_size)
+
+	wg_init_complete := new(sync.WaitGroup)
+	wg_init_complete.Add(cluster_size)
 	for idx := 0; idx < cluster_size; idx++ {
 
 		//Create a server instance
@@ -62,7 +65,7 @@ func main() {
 
 		//Create an anonymous function in a goroutine that starts the server
 		go func() {
-			log.Fatal(mydynamo.ServeDynamoServer(&serverInstance))
+			log.Fatal(mydynamo.ServeDynamoServer(&serverInstance, wg_init_complete))
 			wg.Done()
 		}()
 		nodeInfo := mydynamo.DynamoNode{
@@ -71,6 +74,8 @@ func main() {
 		}
 		dynamoNodeList = append(dynamoNodeList, nodeInfo)
 	}
+
+	wg_init_complete.Wait()
 
 	//Create a duplicate of dynamoNodeList that we can rotate
 	//so that each node has a distinct preference list
